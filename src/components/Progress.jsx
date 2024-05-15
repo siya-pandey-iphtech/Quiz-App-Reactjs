e5c87262fc921a06958d306749d5875e81b05b37
siyapandeyvsp <siyapandeyvsp@gmail.com>
2024-05-15 13:45:19 +0530

    responsive

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,29 +1,28 @@
 export const Progress = ({ selectedAnswers }) => {
   return (
-    <div className="border-2 p-5  bg-white shadow-lg rounded-lg m-5 mr-0 mt-0 min-w-60">
-      <h1 className="text-xl text-center pb-5">Progress</h1>
+    <div className="border-2 p-5  bg-white shadow-lg rounded-lg m-5 mr-0 mt-0  min-w-1">
+      <h1 className="sm:text-xl text-center pb-5 text-sm  ">Progress</h1>
 
-      <div className="grid grid-cols-5 gap-4">
+      <div className="grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 gap-4">
         {selectedAnswers.map((answer, index) => (
           <button
             key={index}
             className={`p-2 rounded-lg text-white ${
               answer !== null ? "bg-teal-700" : "bg-gray-500"
             }`}
-        
           >
             {index + 1}
           </button>
         ))}
       </div>
-      <div className="flex  flex-col justify-center items-start  m-4">
-        <div className="flex items-center">
+      <div className="flex  hidden sm:block  flex-col justify-center items-start  m-4">
+        <div className="  sm:flex items-center ">
           <div className="w-4 h-4 bg-teal-700 mr-2"></div>
-          <span>Attempted</span>
+          <span >Attempted</span>
         </div>
         <div className="flex mt-5 ">
           <div className="w-4 h-4 bg-gray-500 mr-2"></div>
-          <span>Not Attempted</span>
+          <span >Not Attempted</span>
         </div>
       </div>
     </div>
